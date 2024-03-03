681608d082c4a56a4365ec533176bac75f9d5fd3
Joseph Kamil <kamilj@umich.edu>
2024-03-02 22:02:38 -0800

    correct commit

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 // Import necessary packages
 const inquirer = require('inquirer');
 const fs = require('fs');
-const shapes = require('./lib/document.js');
+const shapes = require('./lib/document');
+const Circle = require('./lib/circle');
+const Square = require('./lib/square');
+const Triangle = require('./lib/triangle');
 
 
 // TODO: Create an array of questions for user input
@@ -19,12 +22,12 @@ const questions = [
     {
         type: 'list',
         message: "Choose a shape for your project.",
-        choices: ['circle', 'triangle', 'square'],
+        choices: ['Circle', 'Triangle', 'Square'],
         name: 'shape'
     },
     {
         type: 'input',
-        name: 'shape/color',
+        name: 'shapeColor',
         message: 'What is the color of the shape?',
     }
     
@@ -32,7 +35,20 @@ const questions = [
 
 // Function to write README file
 function writeToFile(fileName, data) {
-    const svg = shapes(data);
+    const svg = shapes.shapes(data);
+    let svgString = '';
+    let shapeChoice;
+        if (data.shape === "Triangle") {
+            shapeChoice = new Triangle();
+            svgString = `<polygon points="150, 18 244, 182 56, 182" fill="${data.color}"/>`;
+        } else if (data.shape === "Square") {
+            shapeChoice = new Square();
+            svgString = `<rect x="73" y="40" width="160" height="160" fill="${data.color}"/>`;
+        } else if (data.shape === 'Circle') {
+            shapeChoice = new Circle();
+            svgString = `<circle cx="150" cy="115" r="80" fill="${data.color}"/>`;
+        }
+
     fs.writeFile(fileName, svg, function (error) {
         if (error) {
             return console.log(error);
@@ -46,7 +62,13 @@ function writeToFile(fileName, data) {
     function init() {
         inquirer.prompt(questions)
             .then((data) => {
-                writeToFile('./logo.svg', data);
+                if (data.text.length > 3) {
+                    console.log("Must enter a value of no more than 3 characters");
+                }
+                else{
+                    writeToFile('logo.svg', data);
+                }
+                
             });
     }
 
